f06f0cf5c5e84e1c65d6f95ece6eff6a686f99a2
steve <steve@b9d65d58-e431-4f38-8355-678f3425cbaf>
2010-01-18 23:07:21 +0000

    handle bundling _staged_ crawl artifacts
    git-svn-id: svn+ssh://home.us.archive.org/srv/svn/draintasker/trunk@18 b9d65d58-e431-4f38-8355-678f3425cbaf

--- bundle-crawl-artifacts.sh
+++ bundle-crawl-artifacts.sh
@@ -3,10 +3,36 @@
 # make a tarball of crawl config, recover, reports, logs, and tombstones
 # and leave in /var/tmp
 #
-#  crawlname  name of crawl job, e.g. GEOCITIES-TEST
-#  jobsdir    crawl job directory, e.g. /0/closure-jobs
-#  bundle     /var/tmp/{crawlname}-{host}-{mtime}-bundle.tar.gz
+# INPUT
 #
+#  crawlname  name of crawl job
+#  jobsdir    crawl job directory
+#  jobroot    staged crawljob root
+#  crawlhost  crawl host (for bundle name when crawldata is staged)
+#
+# OUTPUT
+#
+#  bundle  /var/tmp/{crawlname}-{host}-{mtime}-bundle.tar.gz
+#            config
+#            logs
+#            recover
+#            reports
+#            warcs
+#
+# EXAMPLE
+#
+#  where /staged-crawljobs/STAGED_JOB/[0-3]
+#        -> 0/closure-jobs/CRAWLNAME
+#        -> 1/crawldata/CRAWLNAME
+#        -> 2/crawldata/CRAWLNAME
+#        -> 3/crawldata/CRAWLNAME
+#        -> 1/incoming/CRAWLNAME
+#        -> 3/incoming/CRAWLNAME
+#
+#  crawlname = CRAWLNAME
+#  jobsdir   = /0/closure-jobs
+#  jobroot   = /staged-crawljobs/STAGED_JOB
+# 
 # see also:
 #   make-and-store-bundle.sh
 # examples:
@@ -15,9 +41,9 @@
 # brad 2008 (/home/webcrawl/tools/make-crl-bundle-general)
 # siznax 2009
 
-if [ $# != 2 ]
+if [ $# -lt 2 ]
 then
-  echo "Usage: $0 crawlname jobsdir"
+  echo "Usage: $0 crawlname jobsdir [jobroot] [crawlhost]"
   exit 1
 fi
 
@@ -25,22 +51,46 @@ echo $0 `date`
 
 crawlname=$1
 jobsdir=$2
-jobdir=${jobsdir}/${crawlname}
+
+if [ $3 ]
+then
+  jobroot=$3
+else
+  jobroot=''
+fi
+
+if [ $4 ]
+then
+  host=$4
+else
+  host=`hostname | cut -d '.' -f 1`
+fi
+
+jobdir=${jobroot}/${jobsdir}/${crawlname}
+mtime=`stat -c %y ${jobdir}\
+ | tr -d '-'\
+ | tr -d ':'\
+ | tr -d ' '\
+ | cut -d '.' -f 1`
+bundle=${crawlname}-${host}-${mtime}-bundle.tar.gz
 
 if [ ! -d ${jobdir} ]
 then
   echo "ERROR job not found:" ${jobdir}
   exit 2
+else
+  echo "crawlname: ${crawlname}"
+  echo "jobdir   : ${jobdir}"
+  echo "jobroot  : ${jobroot}"
 fi
 
-dir[0]=${jobdir}
-dir[1]=/1/crawldata/${crawlname}
-dir[2]=/2/crawldata/${crawlname}
-dir[3]=/3/crawldata/${crawlname}
-dir[4]=/0/incoming/${crawlname}
-dir[5]=/1/incoming/${crawlname}
-dir[6]=/2/incoming/${crawlname}
-dir[7]=/3/incoming/${crawlname}
+# potential searchdirs
+dir[0]=${jobdir}                            # config
+dir[1]=${jobroot}/1/crawldata/${crawlname}  # (logs, w/arcs)
+dir[2]=${jobroot}/2/crawldata/${crawlname}  # (state)
+dir[3]=${jobroot}/3/crawldata/${crawlname}  # (w/arcs)
+dir[5]=${jobroot}/1/incoming/${crawlname}   # (tombstones)
+dir[7]=${jobroot}/3/incoming/${crawlname}   # (tombstones)
 
 echo "checking searchdirs"
 i=0
@@ -78,25 +128,35 @@ else
 fi
 
 echo "linking config files into ${config}"
-find ${jobdir} -type f | grep -v scratch | grep -v maps | grep -v '.log$' | grep -v 'report.txt' | xargs -I % ln -s % ${config}
+find ${jobdir} -type f\
+ | grep -v scratch\
+ | grep -v maps\
+ | grep -v '.log$'\
+ | grep -v 'report.txt'\
+ | grep -v '.divert$'\
+ | grep -v '.schedule$'\
+ | xargs -I % ln -s % ${config}
 
 echo "linking logs into ${logs}"
-find ${searchdirs[@]} -type f -name "*.log*" | xargs -I % ln -s % ${logs}
+find ${searchdirs[@]} -type f -name "*.log*"\
+ | xargs -I % ln -s % ${logs}
 
 echo "linking recover files into ${recover}"
-find ${searchdirs[@]} -type f -name "*.recover*" | xargs -I % ln -s % ${recover}
+find ${searchdirs[@]} -type f -name "*.recover*"\
+ | xargs -I % ln -s % ${recover}
 
 echo "linking report files into ${reports}"
-find ${searchdirs[@]} -type f -name "*report.txt*" | xargs -I % ln -s % ${reports}
+find ${searchdirs[@]} -type f -name "*report.txt*"\
+ | xargs -I % ln -s % ${reports}
 
 echo "linking warcs (tombstones) into ${warcs}"
-find ${searchdirs[@]} -type f -name "*.tombstone" | xargs -I % ln -s % ${warcs}
-
-mtime=`stat -c %y ${jobdir} | tr -d '-' | tr -d ':' | tr -d ' ' | cut -d '.' -f 1`
-host=`hostname | cut -d '.' -f 1`
-bundle=${crawlname}-${host}-${mtime}-bundle.tar.gz
+find ${searchdirs[@]} -type f -name "*.tombstone"\
+ | xargs -I % ln -s % ${warcs}
 
 echo "creating bundle: tar -chvf - ${crawlname} | gzip -9 /var/tmp/${bundle}"
-tar -chvf - ${crawlname} | gzip -9 > $bundle 
+tar -chvf - ${crawlname} | gzip -9 > ${bundle}
+
+# cleanup
+rm -rf ${tmpdir}
 
 echo "Done. " `date`
