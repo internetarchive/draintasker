2f4711cc090988bda3b9de042ccd5e6c87c9c169
Kenji Nagahashi <kenji@archive.org>
2012-10-22 21:23:10 +0000

    delete-verified-warcs.sh: stop relying on TOMBSTONE. use .tombstone files instead. fixed misplaced continue for manifest_count == 0 case

--- delete-verified-warcs.sh
+++ delete-verified-warcs.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # delete-verified-warcs.sh xfer_job_dir [force]
 #
-# for each warc_series, if TOMBSTONE found, find each 
-# original_warc in warc_series, and if matching warc_tombstone 
+# for each warc_series, if SUCCESS found, find each 
+# original_warc in warc_series, and if matching .tombstone file
 # found and tombstone_count and manifest_count agree, then 
 # delete original_warc.
 #
 #  xfer_job_dir    /{path}/{job_name}
 #  warc_series     {xfer_job_dir}/{warc_series}
-#  TOMBSTONE       {warc_series}/TOMBSTONE
+#  SUCCESS         {warc_series}/SUCCESS
 #  original_warc   {warc_series}/*.w/arc.gz
 #  warc_tombstone  {warc_series}/*.w/arc.gz.tombstone
 #  [force]         1 = do not query user
@@ -31,21 +31,30 @@ function query_user {
 function clean_item {
   local d=$1
   local warc_series=$(basename $d)
-  local MANIFEST=$d/MANIFEST TOMBSTONE=$d/TOMBSTONE
+  local MANIFEST=$d/MANIFEST
   local upload_files=($(awk '{print $2}' $MANIFEST))
   local manifest_count=${#upload_files[@]}
-  local tombstone_count=$(wc -l < $TOMBSTONE)
 
-  # if MANIFEST and TOMBSTONE does not agree on number of uploaded files,
-  # something must have gone wrong (TODO: can we be a bit smarter here?)
-  if (( tombstone_count != manifest_count )); then
-    printf 'ERROR: count mis-match: TOMBSTONE=%d MANIFEST=%d in series %s\n' \
-	$tombstone_count $manifest_count "$warc_series" >&2
-    return 1
+  # first double check if all files listed in MANIFEST have been uploaded.
+  # if .tombestone's and MANIFEST don't agree, sometthing must have gone
+  # wrong. need to call operator's attention.
+  local missedout=()
+  for w in ${upload_files[@]}; do
+      if [ ! -e $d/$w.tombstone ]; then
+	  missedout+=("$w")
+      fi
+  done
+  if (( ${#missedout[@]} != 0 )); then
+      for w in ${missedout[@]}; do
+	  echo "$w: listed in MANIFEST, but no .tombstone exists" >&2
+      done
+      printf 'ERROR: %d file(s) not uploaded while SUCCESS exists\n' \
+	  ${#missedout[@]} >&2
+      return 1
   fi
   if ((manifest_count == 0)); then
     echo "no uploaded files in this item" >&2
-    continue
+    return 0
   fi
 
   # show what's going to happen if running in interactive mode
@@ -61,11 +70,11 @@ function clean_item {
   for w in ${upload_files[@]}; do
     if [ -e $d/$w ]; then
       # files without corresponding .tombstone file should not be
-      # deleted.
+      # deleted. this is already checked above, but just double-checking.
       t="${w}.tombstone"
       if [ ! -e "$d/$t" ]; then
-	# TODO: should we delete TOMBSTONE to have s3-launch-transfer re-process
-	# this item?
+	# TODO: should we delete SUCCESS etc. to have s3-launch-transfer
+	# re-process this item?
 	echo "ERROR: no .tombstone for $w" >&2
 	return 1
       fi
@@ -73,6 +82,9 @@ function clean_item {
       rm $d/$w || return 1
       (( count_removed++ ))
     else
+      # missing (already deleted) files are okay. this shouldn't happen under
+      # normal situation, but it is sometimes necessary to add/re-upload files
+      # to the item after upload is complete.
       (( count_missing++ ))
     fi
   done
@@ -107,7 +119,7 @@ count_items_locked=0
 for d in $(find $xfer_job_dir -mindepth 1 -maxdepth 1 -type d); do
 
   iid=$(basename $d)
-  TOMBSTONE="$d/TOMBSTONE"
+  SUCCESS="$d/SUCCESS"
   MANIFEST="$d/MANIFEST"
   CLEAN="$d/CLEAN"
 
@@ -120,11 +132,11 @@ for d in $(find $xfer_job_dir -mindepth 1 -maxdepth 1 -type d); do
     continue
   fi
 
-  # s3-launch-transfer.sh creates TOMBSTONE file when it has successfully
-  # uploaded all files. if TOMBSTONE file does not exist, the item is
-  # still being worked on.
-  if [ ! -e $TOMBSTONE ]; then
-    echo "$iid: no TOMBSTONE yet"
+  # s3-launch-transfer.sh creates SUCCESS file when it has successfully
+  # uploaded all files. if SUCCESS file does not exist, the item is
+  # still being worked on or on hold for some error.
+  if [ ! -e $SUCCESS ]; then
+    echo "$iid: no SUCCESS yet"
     (( count_items_active++ ))
     continue
   fi
