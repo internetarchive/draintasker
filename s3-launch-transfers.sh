internetarchive/draintasker

commit fb6d6f175f3bde097e936a88512c02d7eae0bccc

allow for configuring arbitrary item metadata in config. base changes to config.py and s3-launch-transfers.sh. test case for config.py

s3-launch-transfers.sh
@@ -16,7 +16,6 @@
 #     xfer_dir
 #     title_prefix
 #     collection_prefix
-#     test_suffix
 #     block_delay
 #     max_block_count
 #     retry_delay
@@ -40,15 +39,10 @@
 
 PG=$0; test -h $PG && PG=$(readlink $PG)
 BIN=$(dirname $PG)
+: ${GETCONF:=$BIN/config.py}
 
 usage="config [force] [mode=single]"
 
-: ${TEST:=false}
-if $TEST; then
-    CURL=curl_fake
-else
-    CURL=curl
-fi
 s3='s3.us.archive.org'
 dl='www.archive.org/download'
 std_warc_size=$(( 1024*1024*1024 )) # 1 gibibyte
@@ -98,21 +92,6 @@ function set_date_range {
     start_date_ISO="$(set_tISO "$t")"
     start_date_HR="$(set_tHR "$t")"
 
-    # # get warc start date
-    # first_file=`echo ${files[0]} | tr '/' ' ' | awk '{print $NF}'`
-    # if [ $compact_names == 1 ]
-    # then
-    #     t=`echo $files[0] | cut -d '-' -f 2`
-    # else
-    #     t=`echo $files[0] | cut -d '-' -f 2 | grep -o "[0-9]\{14,17\}"`000
-    # 	t=${t:0:17}
-    # fi
-    # first_file_date=$t
-    # set_tISO
-    # start_date_ISO=${tISO}
-    # set_tHR
-    # start_date_HR=${tHR}
-
     scandate=${t:0:14}
     metadate=${t:0:4}
     
@@ -126,38 +105,6 @@ function set_date_range {
     end_date_ISO="$(set_tISO "$t")"
     end_date_HR="$(set_tHR "$t")"
     
-    # if [ ${#files[@]} -gt 1 ]; then
-    # 	last_file=${files[${#files[@]}-1]}
-    #     #last_file=`echo "${files[@]:$((${#files[@]}-1))}"\
-    #     #           | tr '/' ' ' | awk '{print $NF}'`
-    # 	t=
-    #     # if [ $compact_names == 1 ]
-    #     # then
-    #     #     t=`echo $last_file | cut -d '-' -f 2 | grep -o "[0-9]\{14\}"`
-    #     # else
-    #     #     t=`echo $last_file | cut -d '-' -f 2 | grep -o "[0-9]\{14,17\}"`000
-    # 	#     t=${t:0:17}
-    #     # fi
-    #     last_file_date=$t
-    #     # warc end date (from last file mtime). should (closely) 
-    #     # correspond to time of last record in series
-    #     # t=`stat --format=%x "${files[@]:$((${#files[@]}-1))}"\
-    #     #   | cut -d '.' -f 1\
-    #     #   | tr  -d '-'\
-    #     #   | tr  -d ' '\
-    #     #   | tr  -d ':'`
-    # 	t=$(date +%Y%m%d%H%M%S -d @"$(stat -c %Y "$last_file")")
-    # 	last_date=$t
-    # 	end_date_ISO="$(set_tISO "$t")"
-    # 	end_date_HR="$(set_tHR "$t")"
-    # fi
-
-    # if [ -z $end_date_ISO ]
-    # then
-    #     date_range=${start_date_ISO}
-    # else
-    #     date_range="${start_date_ISO} to ${end_date_ISO}"
-    # fi
     date_range="${start_date_ISO} to ${end_date_ISO}"
 }
 
@@ -177,24 +124,22 @@ function echo_curl_output {
 }
 
 function write_success {
-    SUCCESS_FLAG='true'
-    if [ -e "$ERROR" ]
-    then
-        SUCCESS_FLAG='false'
+    local d=$1
+    local success=1
+    if [ -e "$ERROR" ]; then
+	success=0
         echo "ERROR file exists, could not write SUCCESS file." | tee -a $OPEN
     else
-        for warc in `cat $MANIFEST | tr -s ' ' | cut -d ' ' -f 2`
-        do
-            if [ ! -f "${d}/${warc}.tombstone" ]
-            then
-                SUCCESS_FLAG='false'
+	for warc in $(awk '{print $2}' $MANIFEST); do
+            if [ ! -f "${d}/${warc}.tombstone" ]; then
+                #SUCCESS_FLAG='false'
+		success=0
                 echo "ERROR: missing tombstone: ${d}/${warc}" | tee -a $OPEN
                 cp $OPEN $ERROR
                 exit 5
             fi
         done
-        if [ $SUCCESS_FLAG == 'true' ]
-        then
+	if (( success )); then
             echo "copying TASK file:" | tee -a $OPEN
             echo "    $TASK" | tee -a $OPEN
             echo "to SUCCESS file:" | tee -a $OPEN
@@ -243,10 +188,10 @@ function verify_etag {
 }
 
 function write_tombstone {
-    echo "writing download:" | tee -a $OPEN
-    echo "  $download" | tee -a $OPEN
-    echo "into tombstone:" | tee -a $OPEN
-    echo "  $tombstone" | tee -a $OPEN
+    echo "writing download:"
+    echo "  $download"
+    echo "into tombstone:"
+    echo "  $tombstone"
     echo $download > $tombstone
 }
 
@@ -266,7 +211,7 @@ function schedule_retry {
 
 function curl_fake {
     printf '\n>>> THIS IS ONLY A TEST!<<<\n\n' >&2
-    sleep 10
+    sleep 5
     # [response_code] [size_upload] [time_total]
     echo "200 000 000"
 }
@@ -293,12 +238,12 @@ function check_curl_success {
 		bucket_status=1;;
 	    test-add-to-bucket)
 		bucket_status=1
-		echo "creating file: $BUCKET_OK"
-	        touch $BUCKET_OK
+		echo "creating file: $BUCKET_OK" >>$OPEN
+		echo $bucket > $BUCKET_OK
 		;;
 	    *)
 		if verify_etag; then
-                    write_tombstone
+                    write_tombstone >>$OPEN
 		fi
 	    esac
             keep_trying='false'
@@ -353,7 +298,7 @@ function curl_s3 {
             -e 's/ http/\n  &/'\
             -e 's/w\?arc\(.gz\)\?,/&\n    /g'\
             -e 's/-o /\n  &/'\
-      | tee -a $OPEN
+      >>$OPEN
 
     output=$("${curl_cmd[@]}")
     check_curl_success
@@ -369,11 +314,13 @@ if [ -z "$CONFIG" ]; then
     exit 1
 fi
 
-force=$2
-mode=$3
+force=${2:-0}
+mode=${3:-0}
 
-if [ -z $force ]; then force=0; fi
-if [ -z $mode  ]; then mode=0;  fi
+CURL=curl
+if [ $mode = test ]; then
+    CURL=curl_fake
+fi
 
 echo $(basename $0) $(date)
 
@@ -388,15 +335,16 @@ if [ ! -f $S3CFG ]; then
     exit 1
 fi
 # validate configuration
-$BIN/config.py $CONFIG || {
+$GETCONF $CONFIG || {
     echo "ERROR: invalid config: $CONFIG"
     exit 1
 }
-xfer_job_dir=$($BIN/config.py $CONFIG xfer_dir)
+xfer_job_dir=$($GETCONF $CONFIG xfer_dir)
 
-crawljob=$($BIN/config.py $CONFIG crawljob)
-compact_names=$($BIN/config.py $CONFIG compact_names)
-warc_naming=$($BIN/config.py $CONFIG WARC_naming)
+# crawljob is used in description
+crawljob=$($GETCONF $CONFIG crawljob)
+compact_names=$($GETCONF $CONFIG compact_names)
+warc_naming=$($GETCONF $CONFIG WARC_naming)
 if ((compact_names)); then
     WARC_NAME_PATTERN='{prefix}-{timestamp}-{serial}'
 else
@@ -422,10 +370,8 @@ do
   BUCKET_OK="$d/BUCKET_OK"
   tmpfile="/var/tmp/curl.out.$$"
 
-  #warc_series=`echo $d | egrep -o '/([^/]*)$' | tr -d "/"`
   warc_series=$(basename $d)
-  crawler=`echo $warc_series | awk -v FS=- '{print $NF}'`
-  #crawler=`echo $warc_series | tr '-' ' ' | awk '{print $NF}'`
+  #crawler=`echo $warc_series | awk -v FS=- '{print $NF}'`
   crawldata="$d"
 
   # handle (5xx) RETRY file
@@ -545,65 +491,66 @@ do
   secret_key=`grep secret_key $S3CFG | awk '{print $3}'`
 
   # parse config
-  title_prefix=`$BIN/config.py $CONFIG title_prefix`
-  # test_suffix=`$BIN/config.py $CONFIG test_suffix`
-  block_delay=`$BIN/config.py $CONFIG block_delay`
-  max_block_count=`$BIN/config.py $CONFIG max_block_count`
-  retry_delay=`$BIN/config.py $CONFIG retry_delay`
+  title_prefix=`$GETCONF $CONFIG title_prefix`
+  block_delay=`$GETCONF $CONFIG block_delay`
+  max_block_count=`$GETCONF $CONFIG max_block_count`
+  retry_delay=`$GETCONF $CONFIG retry_delay`
 
   # parse series
   #ws_date=`echo $warc_series | cut -d '-' -f 2`
   #cdate=`echo ${ws_date:0:6}`
 
   # bucket metadata
-  bucket="${warc_series}${test_suffix}"
-  mediatype='web'
+  bucket=${warc_series}
   title="${title_prefix} ${date_range}"
-  subject='crawldata'
-  derive=`$BIN/config.py $CONFIG derive`
+  derive=$($GETCONF $CONFIG derive)
 
   num_warcs=${nfiles_manifest}
   size_hint=`cat $PACKED | awk '{print $NF}'`
   first_serial=$(parse_warc_name "$(basename "$first_file")"; echo $serial)
   last_serial=$(parse_warc_name "$(basename "$last_file")"; echo $serial)
-  # if [ $compact_names == 1 ]
-  # then
-  #     first_serial=`echo $first_file\
-  # 	| cut -d '-' -f 3 | sed -e 's/.warc.gz//'`
-  #     last_serial=` echo $last_file\
-  # 	| cut -d '-' -f 3 | sed -e 's/.warc.gz//'`
-  # else
-  #     first_serial=`echo $warc_series | cut -d '-' -f 3`
-  #     last_serial=` echo $warc_series | cut -d '-' -f 4`
-  # fi
 
   # metadata per BK, intended to be like books
   #   Subject: metadata for the web stuff going into the paired archive
   #   Date:  2010-09-18T12:16:00PDT
-  scanner=`hostname -f`
-  creator=`$BIN/config.py $CONFIG creator`
-  sponsor=`$BIN/config.py $CONFIG sponsor`
-  contributor=`$BIN/config.py $CONFIG contributor`
+  metadata=()
+  while read m; do
+      metadata+=("$m")
+  done < <( $GETCONF $CONFIG -m metadata )
   # scandate (using 14-digits of timestamp of first warc in series)
   # metadate (like books, the year)
-  operator=`$BIN/config.py $CONFIG operator`
-  scancenter=`$BIN/config.py $CONFIG scanningcenter`
-  access="http://www.archive.org/details/${bucket}"
   crawler_version=$(warc_software ${files[0]})
-  description=`$BIN/config.py $CONFIG description\
+  description=`$GETCONF $CONFIG description\
     | sed -e s/CRAWLHOST/$scanner/\
       -e s/CRAWLJOB/$crawljob/\
       -e s/START_DATE/"$start_date_HR"/\
       -e s/END_DATE/"$end_date_HR"/\
     | tr -s ' '`
 
+  metadata+=(
+      "x-archive-meta-scanner:${scanner}"
+      "x-archive-meta-access:http://archive.org/details/${bucket}"
+      "x-archive-meta-crawler:${crawler_version}"
+      "x-archive-meta-title:${title}"
+      "x-archive-meta-description:${description}"
+      "x-archive-meta-scandate:${scandate}"
+      "x-archive-meta-date:${metadate}"
+      "x-archive-meta-crawljob:${crawljob}"
+      "x-archive-meta-numwarcs:${num_warcs}"
+      "x-archive-meta-sizehint:${size_hint}"
+      "x-archive-meta-firstfileserial:${first_serial}"
+      "x-archive-meta-firstfiledate:${first_file_date}"
+      "x-archive-meta-lastfileserial:${last_serial}"
+      "x-archive-meta-lastfiledate:${last_file_date}"
+      "x-archive-meta-lastdate:${last_date}"
+  )
   # support multiple arbitrary collections
   # webwidecrawl/collection/serial
   #   => collection3 = webwidecrawl
   #   => collection2 = collection
   #   => collection1 = serial
   COLLECTIONS=()
-  colls=($($BIN/config.py $CONFIG collections | tr '/' ' '))
+  colls=($($GETCONF $CONFIG collections | tr '/' ' '))
   coll_count=${#colls[@]}
   for c in ${colls[@]}
   do
@@ -618,39 +565,44 @@ do
       ((coll_count--))
   done
 
-  if [ -z "$creator" -o -z "$sponsor" -o -z "$contributor" -o \
-       -z "$description" -o -z "$scancenter" -o -z "$operator" ]; then
-      echo "ERROR some null metadata." | tee -a $OPEN
-      echo "Aborting." | tee -a $OPEN
-      exit 1          
-  fi
+  #if [ -z "$creator" -o -z "$sponsor" -o -z "$contributor" -o \
+  #     -z "$description" -o -z "$scancenter" -o -z "$operator" ]; then
+  #    echo "ERROR some null metadata." | tee -a $OPEN
+  #    echo "Aborting." | tee -a $OPEN
+  #    exit 1          
+  #fi
 
   echo "[item metadata]"
-  echo "  mediatype     = $mediatype"
-  echo "  title         = $title"
-  echo "  description   = $description"
-  echo "  collection(s) = ${collection[@]}"
-  echo "  subject       = $subject"
-  echo "[books metadata]"
-  echo "  scanningcenter = '${scancenter}'"
-  echo "  scandate       = '${scandate}'"
-  echo "  scanner        = '${scanner}'"
-  echo "  date           = '${metadate}'"
-  echo "  creator        = '${creator}'"
-  echo "  sponsor        = '${sponsor}'"
-  echo "  contributor    = '${contributor}'"
-  echo "  operator       = '${operator}'"
-  echo "  id...-access   = '${access}'"
-  echo "[crawl metadata]"
-  echo "  crawler         = ${crawler_version}"
-  echo "  crawljob        = $crawljob"
-  echo "  numwarcs        = $num_warcs"
-  echo "  sizehint        = $size_hint"
-  echo "  firstfileserial = $first_serial"
-  echo "  firstfiledate   = $first_file_date"
-  echo "  lastfileserial  = $last_serial"
-  echo "  lastfiledate    = $last_file_date"
-  echo "  lastdate        = $last_date"
+  for m in "${metadata[@]}"; do
+      m=${m#x-archive-meta*-} m=${m/:/ = }
+      echo "  $m"
+  done
+  # echo "[item metadata]"
+  # echo "  mediatype     = $mediatype"
+  # echo "  title         = $title"
+  # echo "  description   = $description"
+  # echo "  collection(s) = ${collection[@]}"
+  # echo "  subject       = $subject"
+  # echo "[books metadata]"
+  # echo "  scanningcenter = '${scancenter}'"
+  # echo "  scandate       = '${scandate}'"
+  # echo "  scanner        = '${scanner}'"
+  # echo "  date           = '${metadate}'"
+  # echo "  creator        = '${creator}'"
+  # echo "  sponsor        = '${sponsor}'"
+  # echo "  contributor    = '${contributor}'"
+  # echo "  operator       = '${operator}'"
+  # echo "  id...-access   = '${access}'"
+  # echo "[crawl metadata]"
+  # echo "  crawler         = ${crawler_version}"
+  # echo "  crawljob        = $crawljob"
+  # echo "  numwarcs        = $num_warcs"
+  # echo "  sizehint        = $size_hint"
+  # echo "  firstfileserial = $first_serial"
+  # echo "  firstfiledate   = $first_file_date"
+  # echo "  lastfileserial  = $last_serial"
+  # echo "  lastfiledate    = $last_file_date"
+  # echo "  lastdate        = $last_date"
 
   if [ $force == 0 ]; then query_user; fi
 
@@ -665,30 +617,10 @@ do
   noderive_opts=(--header 'x-archive-queue-derive:0')
   automakebucket_opts=(--header 'x-amz-auto-make-bucket:1')
   sizehint_opts=(--header "x-archive-size-hint:${size_hint}")
-  itemmeta_opts=(
-      --header "x-archive-meta-mediatype:${mediatype}"
-      --header "x-archive-meta-title:${title}"
-      --header "x-archive-meta-description:${description}"
-      --header "x-archive-meta-subject:${subject}"
-      --header "x-archive-meta-scanner:${scanner}"
-      --header "x-archive-meta-creator:${creator}"
-      --header "x-archive-meta-scandate:${scandate}"
-      --header "x-archive-meta-date:${metadate}"
-      --header "x-archive-meta-sponsor:${sponsor}"
-      --header "x-archive-meta-contributor:${contributor}"
-      --header "x-archive-meta-scanningcenter:${scancenter}"
-      --header "x-archive-meta-operator:${operator}"
-      --header "x-archive-meta-identifier-access:${access}"
-      --header "x-archive-meta-crawler:${crawler_version}"
-      --header "x-archive-meta-crawljob:${crawljob}"
-      --header "x-archive-meta-numwarcs:${num_warcs}"
-      --header "x-archive-meta-sizehint:${size_hint}"
-      --header "x-archive-meta-firstfileserial:${first_serial}"
-      --header "x-archive-meta-firstfiledate:${first_file_date}"
-      --header "x-archive-meta-lastfileserial:${last_serial}"
-      --header "x-archive-meta-lastfiledate:${last_file_date}"
-      --header "x-archive-meta-lastdate:${last_date}"
-  )
+  itemmeta_opts=()
+  for m in "${metadata[@]}"; do
+      itemmeta_opts+=(--header "$m")
+  done
 
   filepath=$MANIFEST
   filename="$(basename $filepath).txt"
@@ -722,8 +654,6 @@ do
       fi
       echo "item/bucket created successfully: $bucket" | tee -a $OPEN
   fi
-  #abort_series=0
-  #bad_etag=0
 
   # 2) run HEAD on item URL to make sure item is ready
   # item creation request above may be sitting in the queue for a while.
@@ -803,10 +733,9 @@ do
   # /CURL S3 <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
   # write success, tombstone files
-  if [ $TEST == "false" ]
-  then
-      write_success
-  fi
+  #if [ $mode != test ]; then
+      write_success $d
+  #fi
 
   # unlock process
   echo "mv open file to LAUNCH: $LAUNCH"
@@ -819,8 +748,7 @@ do
   (( launch_count++ ))
 
   # check mode
-  if [ $mode == 'single' ]
-  then
+  if [ $mode == 'single' -o $mode = test ]; then
     echo "$launch_count buckets filled"
     echo "mode = $mode, exiting normally."
     echo `basename $0` "done." `date`

config.py
@@ -76,8 +76,14 @@ class DrainConfig(object):
         self.__check('title_prefix', lambda x: x != 'TBD Crawldata',
                      'is invalid')
         # creator, sponsor, contributor, scanningcenter not null
+        metadata = self.get_param('metadata')
         for key in ('creator','sponsor','contributor','scanningcenter'):
-            self.__check(key, lambda x: x is not None, 'is missing')
+            #self.__check(key, lambda x: x is not None, 'is missing')
+            # these metadata has been moved to "metadata" submap, which
+            # automatically incorporates values from old parameters.
+            if not metadata.get(key, None):
+                raise ValueError, '%s is missing' % key
+
         # derive is int
         self.check_integer('derive')
         # compact_names is int
@@ -108,6 +114,54 @@ class DrainConfig(object):
             return os.path.join(self.get_param('job_dir'), 'DRAINME')
         if param == 'config':
             return os.path.abspath(self.fname)
+        if param == 'collections':
+            # allow a few different formats
+            v = self.cfg[param]
+            if isinstance(v, list):
+                # lower collection first (IA convention)
+                return '/'.join(reversed(v))
+            if isinstance(v, basestring) and v.find(';') >= 0:
+                # IA-conventional one-string notation
+                return '/'.join(reversed(v.split(';')))
+            return v
+        if param == 'scanner':
+            if param in self.cfg:
+                return self.cfg[param]
+            else:
+                return os.uname()[1]
+        if param == 'metadata':
+            # for backward-compatibility, incorporate top-level
+            # metadata config parameters into metadata dict.
+            # templated/auto-generated metadata, such as title, description
+            # and scandate, are not included, because they are handled
+            # specially. TODO: they should be unified into a single framework.
+            # mediatype and subject used to be hard-coded. now they are
+            # configurable with sensible default values.
+            v = self.cfg[param]
+            if v is None or not isinstance(v, dict):
+                # TODO: we should warn/abort if v is not a dict
+                v = {}
+            # TODO: which metadata to include by default would depend on
+            # item's mediatype.
+            meta = dict(
+                [(name, self.cfg[name])
+                for name in (
+                    'creator',
+                    'sponsor',
+                    'contributor',
+                    'operator',
+                    'scanningcenter'
+                    )
+                if name in self.cfg
+                ], mediatype='web', subject='crawldata',
+                scanner=self['scanner'])
+            # values in "metadata" param take precedence over top-level
+            # metadata.
+            v = self.cfg[param]
+            if isinstance(v, dict):
+                meta.update(v)
+            return meta
+                
         return self.cfg.get(param)
 
     def iteritems(self):
@@ -116,20 +170,55 @@ class DrainConfig(object):
         """
         return self.cfg.iteritems()
 
-    def pprint(self):
-        pprint.pprint(self.cfg)
+    def pprint(self, param=None, format=None, out=sys.stdout):
+        if param is None:
+            pprint.pprint(self.cfg, stream=out)
+        else:
+            v = self.get_param(param)
+            if isinstance(v, dict):
+                if format == 'header':
+                    for key, value in v.iteritems():
+                        if re.search(r'\s', key): continue
+                        if value is None or value == '': continue
+                        for s in format_header(key, value):
+                            print >>out, s
+                else:
+                    for key, value in v.iteritems():
+                        # space in key screws up, so drop it
+                        if re.search(r'\s', key): continue
+                        if value is None:
+                            print >>out, "%s\t"
+                        elif isinstance(value, list):
+                            print >>out, "%s\t%s" % (key, ';'.join(value))
+                        else:
+                            print >>out, "%s\t%s" % (key, value)
+            elif isinstance(v, list):
+                for value in v:
+                    print >>out, value
+            else:
+                print >>out, v if v is not None else ''
         
+def format_header(k, v):
+    if isinstance(v, list):
+        for i, v1 in enumerate(v):
+            yield 'x-archive-meta%02d-%s:%s' % (i+1, k, v1)
+    else:
+        yield 'x-archive-meta-%s:%s' % (k, v if v is not None else '')
+            
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        """ usage """
+    from optparse import OptionParser
+    opt = OptionParser()
+    opt.add_option('-f', dest='format', default=None)
+    opt.add_option('-m', action='store_const', dest='format', const='header',
+                   help='equivalent of -f header')
+    options, args = opt.parse_args()
+    if len(args) < 1:
         print os.path.basename(__file__),  __doc__, __author__
         sys.exit(1)
     else:
-        """ process args """
-        config = DrainConfig(sys.argv[1])
-        if len(sys.argv) == 2:
+        config = DrainConfig(args[0])
+        if len(args) == 1:
             if config.validate():
                 config.pprint()
-        elif len(sys.argv) == 3:
-            v = config.get_param(sys.argv[2])
-            print v if v is not None else ''
+        elif len(args) == 2:
+            config.pprint(param=args[1], format=options.format)

test/test-config.py
@@ -1,0 +1,156 @@
+#!/usr/bin/python
+#
+import sys
+import os
+import re
+import unittest
+import subprocess
+from tempfile import NamedTemporaryFile
+from StringIO import StringIO
+
+from config import DrainConfig
+
+# job_dir and xfer_dir have"/tmp" to keep validate() happy.
+TESTCONF_1 = """
+crawljob: wide
+job_dir: /tmp
+xfer_dir: /tmp
+sleep_time: 300
+max_size: 10
+WARC_naming: 2
+block_delay: 120
+max_block_count: 120
+retry_delay: 2400
+description: CRAWLHOST:CRAWLJOB from START_DATE to END_DATE.
+operator: crawl@archive.org
+collections: webwidecrawl/widecrawl/wide00004
+title_prefix: Webwide Crawldata
+creator: Internet Archive
+sponsor: Internet Archive
+contributor: Internet Archive
+scanningcenter: sanfrancisco
+derive: 1
+compact_names: 0
+metadata:
+  sponsor: Other Sponsor
+  arbitrary: Metadata
+  multivalue:
+    - value1
+    - value2
+    - value3
+"""
+
+TESTCONF_ALTCOL_1 = re.sub(
+    r'(?m)^collections: .*$',
+    'collections: wide00004;widecrawl;webwidecrawl',
+    TESTCONF_1
+    )
+TESTCONF_ALTCOL_2 = re.sub(
+    r'(?m)^collections: .*$',
+    """collections:
+ - wide00004
+ - widecrawl
+ - webwidecrawl""",
+    TESTCONF_1
+    )
+TESTCONF_SUPPRESS = TESTCONF_1 + """  scanningcenter: null
+  scanner: null
+"""
+
+class ConfigTestCase(unittest.TestCase):
+    def _conf(self, s):
+        f = NamedTemporaryFile()
+        f.write(s); f.flush()
+        cf = DrainConfig(f.name)
+        f.close()
+        return cf
+
+    def test1(self):
+        cf = self._conf(TESTCONF_1)
+        # this checks if job_dir exists
+        cf.validate()
+
+        assert cf['creator'] == 'Internet Archive'
+        assert cf['sponsor'] == 'Internet Archive'
+
+    def testCollectionAlternativeFormat1(self):
+        cf = self._conf(TESTCONF_ALTCOL_1)
+        assert cf['collections'] == 'webwidecrawl/widecrawl/wide00004'
+
+    def testCollectionAlternativeFormat2(self):
+        cf = self._conf(TESTCONF_ALTCOL_2)
+        assert cf['collections'] == 'webwidecrawl/widecrawl/wide00004'
+        
+    def testMetadata(self):
+        cf = self._conf(TESTCONF_1)
+
+        md = cf['metadata']
+        assert isinstance(md, dict)
+
+        # metadata automatically incorporate corresponding metadata
+        # specified at the top level. If metadata member has an entry,
+        # it gets higher precedence.
+        assert md['creator'] == 'Internet Archive'
+        #assert md['sponsor'] == 'Internet Archive' # NO
+        assert md['sponsor'] == 'Other Sponsor', md['sponsor']
+        assert md['contributor'] == 'Internet Archive'
+        assert md['operator'] == 'crawl@archive.org'
+        assert md['scanningcenter'] == 'sanfrancisco'
+
+        assert md['scanner'] == os.uname()[1]
+
+        assert md['mediatype'] == 'web'
+        assert md['subject'] == 'crawldata'
+
+        # arbitrary metadata can be specified this way
+        assert md['arbitrary'] == 'Metadata'
+
+    def testPrintHeader(self):
+        cf = self._conf(TESTCONF_1)
+
+        f = StringIO()
+        cf.pprint('metadata', format='header', out=f)
+        s = f.getvalue()
+        f.close()
+
+        headers = {}
+        for l in StringIO(s):
+            m = re.match(r'x-archive.meta(\d*)-([^:]+):(.*)', l.rstrip())
+            assert m, 'unexpected format: %s' % l
+            headers[m.group(2)+m.group(1)] = m.group(3)
+
+        assert headers['creator'] == 'Internet Archive'
+        assert headers['operator'] == 'crawl@archive.org'
+        assert headers['mediatype'] == 'web'
+        assert headers['subject'] == 'crawldata'
+        assert headers['sponsor'] == 'Other Sponsor'
+
+        assert headers['multivalue01'] == 'value1'
+        assert headers['multivalue02'] == 'value2'
+        assert headers['multivalue03'] == 'value3'
+
+        # must not include 'collections'
+        assert 'collections' not in headers
+        assert 'collections01' not in headers
+
+    def testPrintHeaderSuppress(self):
+        """metadata with null/empty value shall not be included in
+        output. this is used for supressing default values."""
+        cf = self._conf(TESTCONF_SUPPRESS)
+
+        f = StringIO()
+        cf.pprint('metadata', format='header', out=f)
+        s = f.getvalue()
+        f.close()
+
+        headers = {}
+        for l in StringIO(s):
+            m = re.match(r'x-archive.meta(\d*)-([^:]+):(.*)', l.rstrip())
+            assert m, 'unexpected format: %s' % l
+            headers[m.group(2)+m.group(1)] = m.group(3)
+
+        assert 'scanningcetner' not in headers, s
+        assert 'scanner' not in headers, s
+
+if __name__ == '__main__':
+    unittest.main()

commit 598dfaf0ff80eabdd86e8106fb7f569b3dabccb9

fix metadata typo introduced in fb6d6f17: access -> identifier-access. also update identifier-access URL to https

s3-launch-transfers.sh
@@ -520,7 +520,7 @@ do
     | tr -s ' '`
 
   metadata+=(
-      "x-archive-meta-access:http://archive.org/details/${bucket}"
+      "x-archive-meta-identifier-access:https://archive.org/details/${bucket}"
       "x-archive-meta-crawler:${crawler_version}"
       "x-archive-meta-title:${title}"
       "x-archive-meta-description:${description}"