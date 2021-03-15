faf45b353951796e8717edf2227017981ae0d22e
Christian Clauss <cclauss@me.com>
2021-03-15 11:04:45 +0100

    A few remaining Python 3 issues

--- admin.py
+++ admin.py
@@ -12,7 +12,7 @@ import threading
 
 class Storage(object):
     def __init__(self, **kwds):
-        for k, v in kwds.iteritems():
+        for k, v in kwds.items():
             setattr(self, k, v)
 
 class WebUI(web.RequestHandler):
--- dtmon.py
+++ dtmon.py
@@ -27,9 +27,20 @@ try:
 except:
     pwd = None
 
+def cmp(x, y):  # https://portingguide.readthedocs.io
+    """
+    Replacement for built-in function cmp that was removed in Python 3
+
+    Compare the two objects x and y and return an integer according to
+    the outcome. The return value is negative if x < y, zero if x == y
+    and strictly positive if x > y.
+    """
+    return (x > y) - (x < y)
+
+
 class Storage(object):
     def __init__(self, **kwds):
-        for k, v in kwds.iteritems():
+        for k, v in kwds.items():
             setattr(self, k, v)
 
 def readfile(fn):
